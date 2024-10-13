28716f20231d595e4e058504d18b3f7b32329f60
Raypeter <raypeter053@gmail.com>
2024-10-13 16:41:30 +0100

    Main commit for new design 1

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,44 +1,45 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
         <div className='min-h-[650px] bg-slate-800 px-4 pt-12 pb-4' style={{ backgroundColor: "#2C2C34" }}>
-            <h1 className='my-5 text-5xl font-bold'>DreamGirls</h1>
+            <h1 className='my-5 text-5xl font-bold text-white'>DreamGirls</h1>
             <div className='pt-11'>
                 <ul className='flex flex-row flex-wrap gap-2'>
                     <li className='w-1/3 md:w-1/4 mb-8'>
-                        <a to='/#' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Play now</a>
+                        <Link to='/#' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Play now</Link>
                     </li>
                     <li className='w-1/3 md:w-1/4 mb-8'>
-                        <a to='/#' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Design</a>
+                        <Link to='/#' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Design</Link>
                     </li>
                     <li className='w-1/3 md:w-1/4 mb-8'>
-                        <a to='/#' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Store</a>
+                        <Link to='/#' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Store</Link>
                     </li>
                     <li className='w-1/3 md:w-1/4 mb-8'>
-                        <a to='/#' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Community</a>
+                        <Link to='/#' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Community</Link>
                     </li>
                     <li className='w-1/3 md:w-1/4 mb-8'>
-                        <a to='/games' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Games</a>
+                        <Link to='/games' className='text-white font-bold hover:text-purple-600 transition-colors p-2 text-2xl'>Games</Link>
                     </li>
                 </ul>
             </div>
             <div className='w-full'>
                 <ul class="flex flex-row gap-4">
                     <li class="">
-                        <a class="" to="/#"><i class="fab fa-1x fa-facebook-f text-blue-400"></i></a>
+                        <Link class="" to="/#"><i class="fab fa-1x fa-facebook-f text-blue-400"></i></Link>
                     </li>
                     <li class="">
-                        <a class="" to="/#"><i class="fab fa-1x fa-twitter text-blue-400"></i></a>
+                        <Link class="" to="/#"><i class="fab fa-1x fa-twitter text-blue-400"></i></Link>
                     </li>
                     <li class="">
-                        <a class="" to="/#"><i class="fab fa-1x fa-linkedin-in text-blue-400"></i></a>
+                        <Link class="" to="/#"><i class="fab fa-1x fa-linkedin-in text-blue-400"></i></Link>
                     </li>
                     <li class="">
-                        <a class="" to="/#"><i class="fab fa-1x fa-instagram text-orange-400"></i></a>
+                        <Link class="" to="/#"><i class="fab fa-1x fa-instagram text-orange-400"></i></Link>
                     </li>
                     <li class="">
-                        <a class="" to="/#"><i class="fab fa-1x fa-youtube text-red-400"></i></a>
+                        <Link class="" to="/#"><i class="fab fa-1x fa-youtube text-red-400"></i></Link>
                     </li>
                 </ul>
             </div>
@@ -53,16 +54,16 @@ const Footer = () => {
                 </p>
                 <ul className='flex flex-row flex-wrap gap-4'>
                     <li>
-                        <a to='#' className='text-white font-bold hover:text-purple-600 transition-colors'>Play now</a>
+                        <Link to='#' className='text-white font-bold hover:text-purple-600 transition-colors'>Play now</Link>
                     </li>
                     <li>
-                        <a to='#' className='text-white font-bold hover:text-purple-600 transition-colors'>Design</a>
+                        <Link to='#' className='text-white font-bold hover:text-purple-600 transition-colors'>Design</Link>
                     </li>
                     <li>
-                        <a to='#' className='text-white font-bold hover:text-purple-600 transition-colors'>Store</a>
+                        <Link to='#' className='text-white font-bold hover:text-purple-600 transition-colors'>Store</Link>
                     </li>
                     <li>
-                        <a to='#' className='text-white font-bold hover:text-purple-600 transition-colors'>Community</a>
+                        <Link to='#' className='text-white font-bold hover:text-purple-600 transition-colors'>Community</Link>
                     </li>
                 </ul>
             </div>
